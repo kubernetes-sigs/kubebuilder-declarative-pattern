03df8316103d34d3cf0035e8d71e9dddebc0a60f
Jeff Johnson <jrjohnson@google.com>
2018-12-09 17:26:02 -0800

    declarative: require a fixed labels for watches
    This should significantly reduce the overhead of an operator (or many)
    watching the apiserver for a all types they deploy.

--- pkg/patterns/declarative/labels.go
+++ pkg/patterns/declarative/labels.go
@@ -2,8 +2,9 @@ package declarative
 
 import (
 	"context"
+	"fmt"
 
-	"sigs.k8s.io/controller-runtime/alpha/patterns/declarative/pkg/manifest"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/declarative/pkg/manifest"
 )
 
 // AddLabels returns an ObjectTransform that adds labels to all the objects
@@ -15,5 +16,14 @@ func AddLabels(labels map[string]string) ObjectTransform {
 		}
 
 		return nil
+	}
+}
+
+// SourceLabel returns a fixed label based on the type and name of the DeclarativeObject
+func SourceLabel(ctx context.Context, o DeclarativeObject) map[string]string {
+	gvk := o.GetObjectKind().GroupVersionKind()
+
+	return map[string]string{
+		fmt.Sprintf("%s/%s", gvk.Group, gvk.Kind): o.GetName(),
 	}
 }
