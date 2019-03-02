2811ba1c0926f7b4485f2fef249744508c6e30e2
Jeff Johnson <jrjohnson@google.com>
2019-03-02 11:48:07 -0800

    declarative,addon: basic dashboard operator w/ walkthrough

---
+++ examples/dashboard-operator/pkg/apis/addons/v1alpha1/dashboard_types.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/addon"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DashboardSpec defines the desired state of Dashboard
+type DashboardSpec struct {
+	addon.CommonSpec
+}
+
+// DashboardStatus defines the observed state of Dashboard
+type DashboardStatus struct {
+	addon.CommonStatus
+}
+
+var _ addon.CommonObject = &Dashboard{}
+
+func (c *Dashboard) ComponentName() string {
+	return "dashboard"
+}
+
+func (c *Dashboard) CommonSpec() addon.CommonSpec {
+	return c.Spec.CommonSpec
+}
+
+func (c *Dashboard) GetCommonStatus() addon.CommonStatus {
+	return c.Status.CommonStatus
+}
+
+func (c *Dashboard) SetCommonStatus(s addon.CommonStatus) {
+	c.Status.CommonStatus = s
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Dashboard is the Schema for the dashboards API
+// +k8s:openapi-gen=true
+type Dashboard struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DashboardSpec   `json:"spec,omitempty"`
+	Status DashboardStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// DashboardList contains a list of Dashboard
+type DashboardList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Dashboard `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Dashboard{}, &DashboardList{})
+}
---
+++ examples/dashboard-operator/pkg/controller/dashboard/dashboard_controller.go
@@ -0,0 +1,99 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dashboard
+
+import (
+	//applicationv1beta1 "github.com/kubernetes-sigs/application/pkg/apis/app/v1beta1"
+<<<<<<< HEAD
+	api "sigs.k8s.io/controller-runtime/alpha/patterns/addon/examples/dashboard-operator/pkg/apis/addons/v1alpha1"
+	"sigs.k8s.io/controller-runtime/alpha/patterns/declarative"
+=======
+>>>>>>> f8720e13... f: import paths on dashboard, walkthrough
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+	api "sigs.k8s.io/kubebuilder-declarative-pattern/examples/dashboard-operator/pkg/apis/addons/v1alpha1"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/addon/pkg/status"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/declarative"
+)
+
+var _ reconcile.Reconciler = &ReconcileDashboard{}
+
+// ReconcileDashboard reconciles a Dashboard object
+type ReconcileDashboard struct {
+	declarative.Reconciler
+}
+
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) *ReconcileDashboard {
+	// TODO: Dynamic labels?
+	labels := map[string]string{
+		"k8s-app": "kubernetes-dashboard",
+	}
+
+	r := &ReconcileDashboard{}
+
+	/*
+		app := applicationv1beta1.Application{
+			Spec: applicationv1beta1.ApplicationSpec{
+				Descriptor: applicationv1beta1.Descriptor{
+					Description: "Kubernetes Dashboard is a general purpose, web-based UI for Kubernetes clusters. It allows users to manage applications running in the cluster and troubleshoot them, as well as manage the cluster itself.",
+					Links:       []applicationv1beta1.Link{{Description: "Project Homepage", Url: "https://github.com/kubernetes/dashboard"}},
+					Keywords:    []string{"dashboard", "addon"},
+				},
+			},
+		}
+	*/
+
+	r.Reconciler.Init(mgr, &api.Dashboard{}, "dashboard",
+		declarative.WithObjectTransform(declarative.AddLabels(labels)),
+		//operators.WithGroupVersionKind(api.SchemeGroupVersion.WithKind("dashboard")),
+		//operators.WithApplication(app),
+	)
+	return r
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r *ReconcileDashboard) error {
+	// Create a new controller
+	c, err := controller.New("dashboard-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to Dashboard
+	err = c.Watch(&source.Kind{Type: &api.Dashboard{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	/*
+		// Watch for changes to deployed objects
+		err = r.WatchAllDeployedObjects(c)
+		if err != nil {
+			return err
+		}
+	*/
+
+	return nil
+}
