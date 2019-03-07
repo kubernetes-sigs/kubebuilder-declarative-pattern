21ffb95e6878c9558e949a8716df602aa9d68b5b
Jeff Johnson <jrjohnson@google.com>
2019-03-07 12:50:51 -0800

    check vendor in

--- hack/ci/test.sh
+++ hack/ci/test.sh
@@ -7,6 +7,4 @@ set -o pipefail
 REPO_ROOT=$(dirname "${BASH_SOURCE}")/../..
 cd "${REPO_ROOT}"
 
-go get -u github.com/golang/dep/cmd/dep
-dep ensure
 go test sigs.k8s.io/kubebuilder-declarative-pattern/pkg/...
