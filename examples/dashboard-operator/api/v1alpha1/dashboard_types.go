e72a1fc0c4bf09eb7235d6d48907824ea591947e
Jeff Johnson <jrjohnson@google.com>
2019-06-28 19:18:26 +0000

    dashboard-operator: update for controller-runtime, kubebuilder updates
    New directory structure, new APIs, oh my!
    
    What's exciting about this change is that the golden file does't need to
    change one bit! This validates our operator is doing about what we
    expect it to.
    
    validated:
    - IMG=<..> go run hack/smoketest.go passes

---
+++ examples/dashboard-operator/api/v1alpha1/dashboard_types.go
@@ -0,0 +1,82 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	addonv1alpha1 "sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/addon/pkg/apis/v1alpha1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DashboardSpec defines the desired state of Dashboard
+type DashboardSpec struct {
+	addonv1alpha1.CommonSpec `json:",inline"`
+	addonv1alpha1.PatchSpec  `json:",inline"`
+}
+
+// DashboardStatus defines the observed state of Dashboard
+type DashboardStatus struct {
+	addonv1alpha1.CommonStatus `json:",inline"`
+}
+
+var _ addonv1alpha1.CommonObject = &Dashboard{}
+var _ addonv1alpha1.Patchable = &Dashboard{}
+
+// +kubebuilder:object:root=true
+
+// Dashboard is the Schema for the dashboards API
+type Dashboard struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DashboardSpec   `json:"spec,omitempty"`
+	Status DashboardStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// DashboardList contains a list of Dashboard
+type DashboardList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Dashboard `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Dashboard{}, &DashboardList{})
+}
+
+func (c *Dashboard) ComponentName() string {
+	return "dashboard"
+}
+
+func (c *Dashboard) CommonSpec() addonv1alpha1.CommonSpec {
+	return c.Spec.CommonSpec
+}
+
+func (c *Dashboard) GetCommonStatus() addonv1alpha1.CommonStatus {
+	return c.Status.CommonStatus
+}
+
+func (c *Dashboard) SetCommonStatus(s addonv1alpha1.CommonStatus) {
+	c.Status.CommonStatus = s
+}
+
+func (c *Dashboard) PatchSpec() addonv1alpha1.PatchSpec {
+	return c.Spec.PatchSpec
+}
