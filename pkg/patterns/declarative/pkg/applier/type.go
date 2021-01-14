baf97128403338b0e596cdea719ff6905242ad1f
Sunny <darkowlzz@protonmail.com>
2021-01-14 15:10:32 +0530

    Embed Applier interface in kubectlClient interface
    Rename Applier interface method from `Applyx` to `Apply` and embed
    the Applier interface in the `kubectlClient` interface.

--- pkg/patterns/declarative/pkg/applier/type.go
+++ pkg/patterns/declarative/pkg/applier/type.go
@@ -5,5 +5,5 @@ import (
 )
 
 type Applier interface {
-	Applyx(ctx context.Context, namespace string, manifest string, validate bool, extraArgs ...string) error
+	Apply(ctx context.Context, namespace string, manifest string, validate bool, extraArgs ...string) error
 }
