27795a3254c3dbace480ddee629ea64f9c23994e
Akihito INOH <akihito-inou@nec.com>
2021-02-10 08:46:04 +0900

    add more tests

--- pkg/patterns/declarative/watch_test.go
+++ pkg/patterns/declarative/watch_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func Test_uniqueGroupVersionKind(t *testing.T) {
-	sa := &unstructured.Unstructured{
+	sa1 := &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"apiVersion": "v1",
 			"kind":       "ServiceAccount",
@@ -21,21 +21,44 @@ func Test_uniqueGroupVersionKind(t *testing.T) {
 			},
 		},
 	}
-	saObj, _ := manifest.NewObject(sa)
+	saObj1, _ := manifest.NewObject(sa1)
+
+	sa2 := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "v1",
+			"kind":       "ServiceAccount",
+			"metadata": map[string]interface{}{
+				"name":      "bar-operator",
+				"namespace": "kube-system",
+			},
+		},
+	}
+	saObj2, _ := manifest.NewObject(sa2)
+
+	dp1 := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "apps/v1",
+			"kind":       "Deployment",
+			"metadata": map[string]interface{}{
+				"name": "frontend111",
+			},
+		},
+	}
+	dpObj1, _ := manifest.NewObject(dp1)
 
 	tests := []struct {
-		name         string
-		inputObjects *manifest.Objects
-		expectSchema []schema.GroupVersionKind
+		name           string
+		inputObjects   *manifest.Objects
+		expectedSchema []schema.GroupVersionKind
 	}{
 		{
 			name: "single object",
 			inputObjects: &manifest.Objects{
 				Items: []*manifest.Object{
-					saObj,
+					saObj1,
 				},
 			},
-			expectSchema: []schema.GroupVersionKind{
+			expectedSchema: []schema.GroupVersionKind{
 				{
 					Group:   "",
 					Version: "v1",
@@ -43,12 +66,72 @@ func Test_uniqueGroupVersionKind(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "double same object",
+			inputObjects: &manifest.Objects{
+				Items: []*manifest.Object{
+					saObj1,
+					saObj1,
+				},
+			},
+			expectedSchema: []schema.GroupVersionKind{
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "ServiceAccount",
+				},
+			},
+		},
+		{
+			name: "double same type of object",
+			inputObjects: &manifest.Objects{
+				Items: []*manifest.Object{
+					saObj1,
+					saObj2,
+				},
+			},
+			expectedSchema: []schema.GroupVersionKind{
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "ServiceAccount",
+				},
+			},
+		},
+		{
+			name: "multiple objects",
+			inputObjects: &manifest.Objects{
+				Items: []*manifest.Object{
+					dpObj1,
+					saObj2,
+				},
+			},
+			expectedSchema: []schema.GroupVersionKind{
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "ServiceAccount",
+				},
+				{
+					Group:   "apps",
+					Version: "v1",
+					Kind:    "Deployment",
+				},
+			},
+		},
+		{
+			name: "empty objects",
+			inputObjects: &manifest.Objects{
+				Items: []*manifest.Object{},
+			},
+			expectedSchema: nil,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			actualSchema := uniqueGroupVersionKind(tt.inputObjects)
-			assert.Equal(t, tt.expectSchema, actualSchema)
+			assert.Equal(t, tt.expectedSchema, actualSchema)
 		})
 	}
 }
