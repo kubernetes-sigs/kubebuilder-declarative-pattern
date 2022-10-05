cea9c6e470f61a494343b273cdc236d6bbddb57d
Justin SB <justinsb@google.com>
2022-10-05 10:21:07 -0400

    applylib applier: create Applier wrapper for applylib
    Also add a test

---
+++ pkg/patterns/declarative/pkg/applier/applylib_test.go
@@ -0,0 +1,93 @@
+package applier
+
+import (
+	"context"
+	"net/http"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery/cached/disk"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/mockkubeapiserver"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/test/httprecorder"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/test/testharness"
+)
+
+func TestApplylibApplier(t *testing.T) {
+	testharness.RunGoldenTests(t, "testdata/applylib", func(h *testharness.Harness, testdir string) {
+		ctx := context.Background()
+
+		k8s, err := mockkubeapiserver.NewMockKubeAPIServer(":0")
+		if err != nil {
+			t.Fatalf("error building mock kube-apiserver: %v", err)
+		}
+
+		k8s.RegisterType(schema.GroupVersionKind{Group: "", Version: "v1", Kind: "Namespace"}, "namespaces", meta.RESTScopeRoot)
+
+		defer func() {
+			if err := k8s.Stop(); err != nil {
+				t.Fatalf("error closing mock kube-apiserver: %v", err)
+			}
+		}()
+
+		addr, err := k8s.StartServing()
+		if err != nil {
+			t.Errorf("error starting mock kube-apiserver: %v", err)
+		}
+
+		klog.Infof("mock kubeapiserver will listen on %v", addr)
+
+		var requestLog httprecorder.RequestLog
+		wrapTransport := func(rt http.RoundTripper) http.RoundTripper {
+			return httprecorder.NewRecorder(rt, &requestLog)
+		}
+		restConfig := &rest.Config{
+			Host:          addr.String(),
+			WrapTransport: wrapTransport,
+		}
+
+		patchOptions := metav1.PatchOptions{FieldManager: "kdp-test"}
+		applier := NewApplySetApplier(patchOptions)
+
+		if h.FileExists(filepath.Join(testdir, "before.yaml")) {
+			before := string(h.MustReadFile(filepath.Join(testdir, "before.yaml")))
+			if err := k8s.AddObjectsFromManifest(before); err != nil {
+				t.Fatalf("error precreating objects: %v", err)
+			}
+		}
+		manifest := string(h.MustReadFile(filepath.Join(testdir, "manifest.yaml")))
+
+		tmpdir := h.TempDir()
+		discoveryCacheDir := filepath.Join(tmpdir, "discoverycache")
+		httpCacheDir := filepath.Join(tmpdir, "httpcache")
+		ttl := 10 * time.Minute
+
+		cachedDiscoveryClient, err := disk.NewCachedDiscoveryClientForConfig(restConfig, discoveryCacheDir, httpCacheDir, ttl)
+		if err != nil {
+			h.Fatalf("error from NewCachedDiscoveryClientForConfig: %v", err)
+		}
+		restMapper := restmapper.NewDeferredDiscoveryRESTMapper(cachedDiscoveryClient)
+		options := ApplierOptions{
+			Manifest:   manifest,
+			RESTConfig: restConfig,
+			RESTMapper: restMapper,
+		}
+
+		if err := applier.Apply(ctx, options); err != nil {
+			t.Fatalf("error from applier.Apply: %v", err)
+		}
+
+		t.Logf("replacing old url prefix %q", "http://"+restConfig.Host)
+		requestLog.ReplaceURLPrefix("http://"+restConfig.Host, "http://kube-apiserver")
+		requestLog.RemoveUserAgent()
+		requests := requestLog.FormatHTTP()
+
+		h.CompareGoldenFile(filepath.Join(testdir, "expected.yaml"), requests)
+	})
+}
