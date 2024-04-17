f5049644330a92141f05ac2cf69993d1d2c6dffe
justinsb <justinsb@google.com>
2024-04-16 22:16:31 -0400

    mockkubeapiserver: Support stringData when creating a secret
    This is an edge case in the kube apiserver, but there is special
    handling for the stringData field of a secret, that is mapped to
    base64 data.
    
    Co-authored-by: Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>

--- mockkubeapiserver/go.mod
+++ mockkubeapiserver/go.mod
@@ -7,14 +7,17 @@ toolchain go1.22.0
 require (
 	github.com/google/go-cmp v0.6.0
 	k8s.io/apimachinery v0.26.2
+	k8s.io/client-go v0.26.2
 	k8s.io/klog/v2 v2.110.1
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1
 	sigs.k8s.io/yaml v1.4.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -22,11 +25,16 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/oauth2 v0.12.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/client-go v0.26.2 // indirect
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 )
---
+++ mockkubeapiserver/tests/kubeapiserver_test.go
@@ -0,0 +1,94 @@
+package applier
+
+import (
+	"context"
+	"net/http"
+	"path/filepath"
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/mockkubeapiserver"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/declarative/pkg/manifest"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/restmapper"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/test/httprecorder"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/test/testharness"
+)
+
+func TestGoldenTests(t *testing.T) {
+	testharness.RunGoldenTests(t, "testdata", func(h *testharness.Harness, testdir string) {
+		ctx := context.Background()
+
+		k8s, err := mockkubeapiserver.NewMockKubeAPIServer(":0")
+		if err != nil {
+			t.Fatalf("error building mock kube-apiserver: %v", err)
+		}
+		defer func() {
+			if err := k8s.Stop(); err != nil {
+				t.Fatalf("error closing mock kube-apiserver: %v", err)
+			}
+		}()
+
+		addr, err := k8s.StartServing()
+		if err != nil {
+			t.Errorf("error starting mock kube-apiserver: %v", err)
+		}
+
+		klog.Infof("mock kubeapiserver will listen on %v", addr)
+
+		var requestLog httprecorder.RequestLog
+		wrapTransport := func(rt http.RoundTripper) http.RoundTripper {
+			return httprecorder.NewRecorder(rt, &requestLog)
+		}
+		restConfig := &rest.Config{
+			Host:          addr.String(),
+			WrapTransport: wrapTransport,
+		}
+
+		httpClient := &http.Client{
+			Transport: wrapTransport(http.DefaultTransport),
+		}
+
+		p := filepath.Join(testdir, "manifest.yaml")
+		manifestYAML := string(h.MustReadFile(p))
+		objects, err := manifest.ParseObjects(ctx, manifestYAML)
+		if err != nil {
+			t.Errorf("error parsing manifest %q: %v", p, err)
+		}
+
+		restMapper, err := restmapper.NewForTest(restConfig)
+		if err != nil {
+			t.Fatalf("error from controllerrestmapper.NewForTest: %v", err)
+		}
+
+		dynamicClient, err := dynamic.NewForConfigAndClient(restConfig, httpClient)
+		if err != nil {
+			t.Fatalf("building dynamic client: %v", err)
+		}
+		for _, obj := range objects.GetItems() {
+			gvk := obj.GroupVersionKind()
+			restMapping, err := restMapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+			if err != nil {
+				t.Errorf("error getting restmapping for %v: %v", gvk, err)
+			}
+
+			var applyOptions metav1.ApplyOptions
+			applyOptions.FieldManager = "test"
+			resource := dynamicClient.Resource(restMapping.Resource).Namespace(obj.GetNamespace())
+
+			if _, err := resource.Apply(ctx, obj.GetName(), obj.UnstructuredObject(), applyOptions); err != nil {
+				t.Fatalf("error applying resource %v: %v", gvk, err)
+			}
+		}
+
+		t.Logf("replacing old url prefix %q", "http://"+restConfig.Host)
+		requestLog.ReplaceURLPrefix("http://"+restConfig.Host, "http://kube-apiserver")
+		requestLog.RemoveUserAgent()
+		requestLog.SortGETs()
+
+		requests := requestLog.FormatHTTP()
+		h.CompareGoldenFile(filepath.Join(testdir, "expected.yaml"), requests)
+	})
+}
